c2ac560ea540dcd7dfc8b395b3b449abd25af47d
Anushka Sharma <anushka626sata@gmail.com>
2025-01-24 12:54:27 -0500

    create the all submissions page

---
+++ frontend/src/pages/allSubmissions.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SubmissionsList from "../components/SubmissionsList";
+
+const AllSubmissionsPage = () => {
+    return (
+        <div>
+            <h1>All Submissions</h1>
+            <SubmissionsList />
+        </div>
+    );
+};
+
+export default AllSubmissionsPage;
