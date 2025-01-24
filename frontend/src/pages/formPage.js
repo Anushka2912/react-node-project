469d5e374286e04efa006d3b169e90c81140d424
Anushka Sharma <anushka626sata@gmail.com>
2025-01-24 12:53:41 -0500

    create the form submission page

---
+++ frontend/src/pages/formPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SubmissionForm from "../components/SubmissionForm";
+
+const FormPage = () => {
+    return (
+        <div>
+            <h1>Form Submission</h1>
+            <SubmissionForm />
+        </div>
+    );
+};
+
+export default FormPage;
