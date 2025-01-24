0f1a748f90b7947987e85b4d0284790a2e83de72
Anushka Sharma <anushka626sata@gmail.com>
2025-01-24 12:56:41 -0500

    update app.js with routes of form page and all submissions page

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,11 +1,17 @@
 import React from "react";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import FormPage from "./pages/formPage";
+import AllSubmissionsPage from "./pages/allSubmissions";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to My React App!</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<FormPage />} />
+        <Route path="/all" element={< AllSubmissionsPage />} />
+      </Routes>  
+    </Router>
   );
-}
+};
 
 export default App;
